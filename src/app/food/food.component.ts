9e7dbf451a77ad946776b9eaf766acb65807bb13
Zenil Haria <zenil.haria@somaiya.edu>
2019-11-12 19:19:53 +0530

    Basic addition done.

--- src/app/food/food.component.ts
+++ src/app/food/food.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-food',
@@ -6,10 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./food.component.css']
 })
 export class FoodComponent implements OnInit {
+  food_object:any;
+  food_list:any;
+  confirm_order_food:any;
+  added:boolean;
+  confirmed:boolean;
+  confirm_food_list:any;
+  constructor() {
+    this.confirm_order_food = {
+      name : '',
+      quantity : 0.0,
+      restaurant : ''
+    }
+    this.added = false;
+  }
 
-  constructor() { }
+  addFood(event, food) {
+    this.confirm_order_food = {
+      name : food.name,
+      quantity : food.quantity,
+      restaurant : food.restaurant
+    };
+    this.added = true;
+  }
+
+  confirm(event) {
+    const confirm_order_food = this.confirm_order_food;
+    this.food_list = this.food_list.filter(function(food) {
+      return food.name != confirm_order_food.name || food.quantity != confirm_order_food.quantity || food.restaurant != confirm_order_food.restaurant
+    })
+    this.added = false;
+    this.confirmed = true;
+    this.confirm_food_list.push(confirm_order_food)
+  }
 
   ngOnInit() {
+    this.food_list = [
+      {
+        name:'Pav Bhaji',
+        quantity:3.5,
+        restaurant:'Shiv Sagar',
+      },
+      {
+        name:'Chole Bhature',
+        quantity:6.9,
+        restaurant:'Bhagat Tarachand',
+      }
+    ];
+    this.confirm_food_list = [];
+    this.added = false;
   }
 
 }
